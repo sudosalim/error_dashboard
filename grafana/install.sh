c5b0917604e80e174f7fb530f4db9682c8ef18ac
Salim Salim <salim.salim@couchbase.com>
2024-01-23 20:18:02 +0000

    Fix grafana provisioning

--- grafana/install.sh
+++ grafana/install.sh
@@ -1,11 +1,4 @@
-# create a persistent volume for your data
-docker volume create grafana-storage
+# Until docker is updated
+docker volume create --name=grafana-storage
 
-# start grafana by using the above persistent storage
-# and defining environment variables
- docker run --rm --mount type=bind,source=$PWD/custom.ini,target=/etc/grafana/grafana.ini -p 3000:3000 grafana/grafana
-
-# docker run -d -p 3000:3000 --name=grafana \
-#   --volume grafana-storage:/var/lib/grafana \
-#   -e "GF_SERVER_ROOT_URL=http://my.grafana.server/" \
-#   grafana/grafana-enterprise
+docker-compose up -d
